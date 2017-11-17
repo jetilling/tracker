8b84aa4928016a025d400d7f46a2b35565ba073a
jetilling <jetilling@hotmail.com>
2017-11-16 18:24:57 -0700

    Removed js files from repo

--- start.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var application_1 = require("./application");
-var port = 9001;
-var api = new application_1.WebApi(express(), port);
-api.run();
-//# sourceMappingURL=start.js.map
